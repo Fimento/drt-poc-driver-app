b39bb6180f46336307bd1b9a236db47564114e68
Bruno Godefroy <godefroy.bruno77@gmail.com>
2022-03-18 10:09:04 +0100

    Fetch location from GPS every 5 seconds

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { getCurrentPositionAsync, LocationObject, useBackgroundPermissions } from 'expo-location';
 
 export default function App() {
+  const [status, requestPermission] = useBackgroundPermissions();
+  const [location, setLocation] = useState<LocationObject | undefined>();
+
+  useEffect(() => {
+    if (!status?.granted) return;
+
+    const refreshLocation = async () => {
+      const gpsLocation = await getCurrentPositionAsync();
+      setLocation(gpsLocation);
+    };
+
+    const intervalId = setInterval(refreshLocation, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [status]);
+
   return (
     <View style={styles.container}>
       <Text>DTR PoC driver APP</Text>
+      {location && <Text>{JSON.stringify(location)}</Text>}
+      {!status?.granted && <Button onPress={requestPermission} title="Give permission" />}
       <StatusBar style="auto" />
     </View>
   );
