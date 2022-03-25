be3b85350b8d391bd7387f34bec0c234a38da645
Bruno Godefroy <godefroy.bruno77@gmail.com>
2022-03-25 11:53:52 +0100

    Update position while app is in the background

--- App.tsx
+++ App.tsx
@@ -1,36 +1,71 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { getCurrentPositionAsync, LocationObject, useBackgroundPermissions } from 'expo-location';
+import { startLocationUpdatesAsync, LocationObject, useForegroundPermissions, watchPositionAsync, stopLocationUpdatesAsync, Accuracy, LocationSubscription } from 'expo-location';
+import { defineTask } from 'expo-task-manager';
 import MapView, { Marker } from 'react-native-maps';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+const LOCATION_TASK_NAME = 'background-location-task';
+
+defineTask<{
+  locations: LocationObject[];
+}>(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const latestLocation = data.locations[data.locations.length -1];
+  fetch('http://192.168.14.112:3000', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(latestLocation),
+  }).catch((e) => console.error(e));
+});
+
 export default function App() {
-  const [status, requestPermission] = useBackgroundPermissions();
+  const [status, requestPermission] = useForegroundPermissions();
   const [location, setLocation] = useState<LocationObject | undefined>();
 
   useEffect(() => {
     if (!status?.granted) return;
 
-    const refreshLocation = async () => {
-      const gpsLocation = await getCurrentPositionAsync();
-      fetch('http://192.168.14.112:3000', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(gpsLocation),
-      }).catch((e) => console.error(e));
-      setLocation(gpsLocation);
-    };
-
-    refreshLocation();
-    const intervalId = setInterval(refreshLocation, 5000);
+    startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      accuracy: Accuracy.BestForNavigation,
+      timeInterval: 0,
+      deferredUpdatesInterval: 5000,
+      deferredUpdatesDistance: 0,
+      distanceInterval: 0,
+      foregroundService: {
+        notificationTitle: "We are monitoring your position",
+        notificationBody: "We will let you know when a new trip is available",
+        notificationColor: "#0000FF",
+      },
+    }).catch((e) => console.error(e));
 
     return () => {
-      clearInterval(intervalId);
-    };
+      stopLocationUpdatesAsync(LOCATION_TASK_NAME).catch((e) => console.error(e));
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (!status?.granted) return;
+
+    let locationSubscription: LocationSubscription | undefined;
+    watchPositionAsync({
+      accuracy: Accuracy.BestForNavigation,
+      timeInterval: 5000,
+    }, (gpsLocation) => {
+      setLocation(gpsLocation);
+    }).then((subscription) => locationSubscription = subscription);
+
+    return () => {
+      if (locationSubscription) locationSubscription.remove();
+    }
   }, [status]);
 
   return (
