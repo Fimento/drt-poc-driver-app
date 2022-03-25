a2d50847afe64b250c8aa596aad30d567fb9fe89
Bruno Godefroy <godefroy.bruno77@gmail.com>
2022-03-25 10:04:14 +0100

    Add small server for debugging locally

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  req.on('data', (chunk) => console.log(chunk.toString('utf8')));
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
